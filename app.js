3dab5f88381c2f5c1773c58db445d06d907cfac4
Rukmal Weerawarana <rukmal.weerawarana@gmail.com>
2014-04-03 03:23:46 -0700

    Added a leap motion testing page, with minimal socket.io

--- app.js
+++ app.js
@@ -23,7 +23,16 @@ if ('development' == app.get('env')) {
 }
 
 app.get('/', routes.index);
+app.get('/leaptest', routes.leaptest);
 
-http.createServer(app).listen(app.get('port'), function(){
+var server = http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
+});
+
+var io = require('socket.io').listen(server);
+
+io.sockets.on('connection', function(socket) {
+    socket.on('leap-data', function(data) {
+        console.log(data);
+    });
 });
--- routes/index.js
+++ routes/index.js
@@ -6,3 +6,7 @@
 exports.index = function(req, res){
   res.render('index', { title: 'Leap Pong' });
 };
+
+exports.leaptest = function(req, res) {
+    res.render('leaptest', { title: 'Leap Testing' });
+};
