ef28c99e656a3ea217c03a410eb51c2a779a1a38
Grant Timmerman <granttimmerman@gmail.com>
2014-04-04 12:47:44 -0700

    Added game with movePaddle function. Fixes #5

---
+++ public/js/Game.js
@@ -0,0 +1,54 @@
+function Game () {
+  // Constants
+
+  var MOVE_SPEED = 3;
+
+  // Fields
+
+  // Scores
+  this.scores = {
+    enemy: 0,
+    player: 0
+  };
+
+  // Paddles
+  this.paddles = {
+    enemy: undefined,
+    player: undefined
+  };
+
+  this.hasStarted = false;
+
+  // Methods
+
+  this.setup = function () {
+    this.hasStarted = true;
+
+    this.paddles.enemy = {
+      element: $('.enemy.paddle')
+    };
+    this.paddles.player = {
+      element: $('.player.paddle')
+    };
+  };
+
+  this.movePaddle = function (direction) {
+    var windowWidth = parseFloat($(window).width());
+
+    var $paddle = this.paddles.player.element;
+    var paddleWidth = parseFloat($paddle.width());
+    var paddlePercentWidth = (100 * paddleWidth) / windowWidth;
+
+    var x = parseFloat($paddle.css('left'));
+    var xRatio = x / windowWidth;
+    var percentX = 100 * xRatio;
+    var newPercentX;
+    if (direction === 'left') {
+      newPercentX = percentX - MOVE_SPEED;
+    } else {
+      newPercentX = percentX + MOVE_SPEED;
+    }
+    newPercentX = Math.min(100 - paddlePercentWidth, Math.max(0, newPercentX));
+    $paddle.css('left', newPercentX + '%');
+  };
+}
--- public/js/main.js
+++ public/js/main.js
@@ -4,9 +4,30 @@ $(function() {
   var $popup = $('.popup');
   var $game = $('.game');
 
+  // Setup
+  var game = new Game();
+
+  // Popup controls
   $popup.find('button').click(function () {
     var $button = $(this);
+    game.setup();
 
     $popup.fadeOut(FADE_TIME);
+  });
+
+  // Keyboard controls
+  $(window).keydown(function (e) {
+    if (game.hasStarted) {
+      var ascii = e.which;
+      var left = ascii === 37;
+      var right = ascii === 39;
+
+      if (left) {
+        game.movePaddle('left');
+      }
+      if (right) {
+        game.movePaddle('right');
+      }
+    }
   });
 });
