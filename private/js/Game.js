93baaf4070ded41643afbecb22f75d59d6f68d63
Grant Timmerman <granttimmerman@gmail.com>
2014-04-05 13:58:29 -0700

    Added more server-side game methods

--- private/js/Game.js
+++ private/js/Game.js
@@ -3,13 +3,31 @@
  */
 function Game (server) {
   // Constants
+  var BALL_SPEED = 5;
 
   // Fields
+  var game = this;
+
+  // Scores
+  this.scores = {
+    player1: 0,
+    player2: 0
+  };
 
   // Ball
   this.ball = {
     x: 0,
-    y: 0
+    y: 0,
+    vx: 0,
+    vy: 1,
+    setXY: function (x, y) {
+      this.x = x;
+      this.y = y;
+    },
+    reset: function (x, y) {
+      this.x = 50;
+      this.y = 50;
+    }
   };
 
   // Paddles
@@ -26,8 +44,48 @@ function Game (server) {
         socket.on('leap-data', function(data) {
             console.log(data);
         });
+
+        socket.broadcast('update', game.toJSON());
     });
   })();
+
+  // Methods
+
+  /**
+   * The main method to update the game
+   */
+  this.updateGame = function () {
+    // Update the ball position
+    this.ball.x += this.ball.vx;
+    this.ball.y += this.ball.vy;
+
+    // If the ball is out of bounds, update the score
+    if (ball.y < 0) {
+      ++this.scores.player2;
+      this.reset();
+    } else if (ball.y > 100) {
+      ++this.scores.player1;
+      this.reset();
+    }
+  };
+
+  /**
+   * Resets the game
+   */
+  this.reset = function () {
+    this.ball.reset();
+  };
+
+  /**
+   * Gets a JSON representation of the game state
+   * @returns {Object} The game as an object
+   */
+  this.toJSON = function () {
+    return {
+      ball: this.ball,
+      paddles: this.paddles
+    };
+  };
 }
 
 module.exports = Game;
