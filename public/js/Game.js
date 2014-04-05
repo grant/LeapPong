c1ca6dfb3a626b79ab4e65ae570079ad4e3022c7
Grant Timmerman <granttimmerman@gmail.com>
2014-04-05 12:21:32 -0700

    Added setX for paddle

--- public/js/Game.js
+++ public/js/Game.js
@@ -27,31 +27,45 @@ function Game () {
   this.setup = function () {
     this.hasStarted = true;
 
+    // X is from 0 to 100
     this.paddles.enemy = {
-      element: $('.enemy.paddle')
+      element: $('.enemy.paddle'),
+      x: 50
     };
     this.paddles.player = {
-      element: $('.player.paddle')
+      element: $('.player.paddle'),
+      x: 50
     };
   };
 
   this.movePaddle = function (paddleName, direction) {
     var windowWidth = parseFloat($(window).width());
+    var paddle = this.paddles[paddleName];
+    var $paddle = paddle.element;
 
-    var $paddle = this.paddles[paddleName].element;
+    var x = paddle.x;
+    var xRatio = x / windowWidth;
+    var newX;
+    if (direction === 'left') {
+      newX = x - MOVE_SPEED;
+    } else {
+      newX = x + MOVE_SPEED;
+    }
+    this.setPaddleX(paddleName, newX);
+  };
+
+  this.setPaddleX = function (paddleName, x) {
+    var windowWidth = parseFloat($(window).width());
+    var paddle = this.paddles[paddleName];
+    var $paddle = paddle.element;
     var paddleWidth = parseFloat($paddle.width());
     var paddlePercentWidth = (100 * paddleWidth) / windowWidth;
 
-    var x = parseFloat($paddle.css('left'));
-    var xRatio = x / windowWidth;
-    var percentX = 100 * xRatio;
-    var newPercentX;
-    if (direction === 'left') {
-      newPercentX = percentX - MOVE_SPEED;
-    } else {
-      newPercentX = percentX + MOVE_SPEED;
-    }
-    newPercentX = Math.min(100 - paddlePercentWidth, Math.max(0, newPercentX));
-    $paddle.css('left', newPercentX + '%');
+    var newX = Math.min(100, Math.max(0, x));
+    paddle.x = newX;
+
+    var xRatio = newX / 100;
+    var leftOffset = xRatio * (100 - paddlePercentWidth);
+    $paddle.css('left', leftOffset + '%');
   };
 }
